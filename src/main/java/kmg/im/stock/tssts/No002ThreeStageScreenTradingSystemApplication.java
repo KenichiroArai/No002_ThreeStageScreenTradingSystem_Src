8e791c3da574e989fdd1cbcb06d8b8330980c303
KenichiroArai <sammyleejunior@gmail.com>
2021-04-05 23:54:54 +0900

    (refs #3914) 新規プロジェクトの作成

---
+++ src/main/java/kmg/im/stock/tssts/No002ThreeStageScreenTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package kmg.im.stock.tssts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class No002ThreeStageScreenTradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(No002ThreeStageScreenTradingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kmg/im/stock/tssts/No002ThreeStageScreenTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kmg.im.stock.tssts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class No002ThreeStageScreenTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
